1bb26f14edf1653ff004cef6426184e0c7864f00
Robert Bovee <Rjbovee112@gmail.com>
2020-04-18 15:10:54 -0600

    tyring to connect to heroku

---
+++ build/precache-manifest.3c1a2df4c9d1ad9e0d8af69687a9dcb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ee2f4d4ff6d76831ddb0a0705bbb75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40a342a35ebd5b8e57b6",
+    "url": "/static/css/main.2042399c.chunk.css"
+  },
+  {
+    "revision": "324157d7a4890d3a5c76",
+    "url": "/static/js/2.cf18b09e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf18b09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a342a35ebd5b8e57b6",
+    "url": "/static/js/main.9f0631fc.chunk.js"
+  },
+  {
+    "revision": "70ddb2070a6c18c75b5f",
+    "url": "/static/js/runtime-main.6cc1646d.js"
+  }
+]);
